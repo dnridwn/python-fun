9c9e43ef6658024137f1dd56034f13c452e8ac18
Den Ridwan Saputra <44092980+dnridwn@users.noreply.github.com>
2023-01-22 01:32:29 +0700

    Update find_index_with_binary_search.py

--- find_index_with_binary_search.py
+++ find_index_with_binary_search.py
@@ -1,6 +1,6 @@
 import math
 
-def find_index_with_binary_search(number, numbers):
+def find_index_with_binary_search(numberToFind, numbers):
     if not isinstance(numbers, list):
         return "Invalid list of numbers"
     
@@ -8,13 +8,13 @@ def find_index_with_binary_search(number, numbers):
     mid_index = len(numbers) // 2
     last_index = len(numbers) - 1
     
-    if number < numbers[first_index] or number > numbers[last_index]:
+    if numberToFind < numbers[first_index] or numberToFind > numbers[last_index]:
         return "Number out of range"
     
-    while number != numbers[mid_index]:
-        if number <= numbers[mid_index]:
+    while numberToFind != numbers[mid_index]:
+        if numberToFind <= numbers[mid_index]:
             last_index = mid_index
-        elif number >= numbers[mid_index]:
+        elif numberToFind >= numbers[mid_index]:
             first_index = mid_index
             
         mid_index = (math.ceil((last_index - first_index) / 2)) + first_index
